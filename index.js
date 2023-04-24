4c12bd615896ad1b5e3fb57f28909c500abc365b
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-24 04:20:22 -0500

    improve scalability

--- index.js
+++ index.js
@@ -1,58 +1,75 @@
 class Book {
-    constructor(Title, Author) {
-        this.Title = Title
-        this.Author = Author
-    }
+  constructor(Title, Author) {
+    this.Title = Title;
+    this.Author = Author;
+  }
 }
 
-const add = document.getElementById('add')
-const title = document.getElementById('title')
-const author = document.getElementById('author')
-const bookshelf = document.querySelector('.bookshelf')
+const add = document.getElementById('add');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const bookshelf = document.querySelector('.bookshelf');
 
-const awesomeBooks = localStorage.getItem('Books') ? JSON.parse(localStorage.getItem('Books')) : []; 
+const awesomeBooks = localStorage.getItem('Books') ? JSON.parse(localStorage.getItem('Books')) : [];
 
+function createBookInfo(info) {
+  const pBook = document.createElement('p');
+  pBook.textContent = info;
+  return pBook;
+}
+
+function createButton(func, textContent, data, classes, index) {
+  const button = document.createElement('button');
+  button.type = func;
+  button.classList.add(`${classes}`);
+  button.setAttribute(`data-${data}`, index);
+  button.textContent = textContent;
+  return button;
+}
+
+function createBookDiv(title, author, index) {
+  const div = document.createElement('div');
+  div.setAttribute('data-book', `${index}`);
+  div.append(createBookInfo(title), createBookInfo(author));
+  div.appendChild(createButton('button', 'Remove', 'index', 'removeBtn', index));
+  return div;
+}
 
 // Displaying awesome books
 const displayBooks = () => {
-    bookshelf.innerHTML = '';
-     awesomeBooks.forEach((book, i) => {
-     bookshelf.innerHTML += `
-         <div>
-             <p>${book.Title}</p>
-             <p>${book.Author}</p>
-             <button type="button" class="removeBtn" data-index="${i}">Remove</button>
-             <hr>   
-         </div>
-     `;
-     bookshelf.value = '';
-    })
-}
-
-displayBooks()
+  const bookshelfFrag = document.createDocumentFragment();
+  awesomeBooks.forEach((book, i) => {
+    bookshelfFrag.appendChild(createBookDiv(book.Title, book.Author, i));
+  });
+  bookshelf.appendChild(bookshelfFrag);
+};
 
 // addding to awesome books
 const addBook = () => {
-    const book = new Book(title.value.trim(), author.value.trim())
-    if (title.value.length && author.value.length) { awesomeBooks.push(book); }
-    localStorage.setItem('Books', JSON.stringify(awesomeBooks));
-    title.value = '';
-    author.value = '';
-}
+  const book = new Book(title.value.trim(), author.value.trim());
+  const bookshelfArrLen = document.querySelectorAll('.bookshelf div').length;
+  if (title.value.length && author.value.length) { awesomeBooks.push(book); }
+  localStorage.setItem('Books', JSON.stringify(awesomeBooks));
+  bookshelf.appendChild(createBookDiv(title.value, author.value, bookshelfArrLen));
+  title.value = '';
+  author.value = '';
+};
+
+window.addEventListener('load', displayBooks);
 
 // even listener on add button
 add.addEventListener('click', (e) => {
-    e.preventDefault();
-    addBook()
-    displayBooks()
-})
+  e.preventDefault();
+  addBook();
+});
 
 // removing from awesome books
-bookshelf.addEventListener('click', (event) => {
-    if (event.target.classList.contains('removeBtn')) {
-        const index = parseInt(event.target.dataset.index, 10)
-        const newData = awesomeBooks.filter((_book, i) => i!==index)
-        localStorage.setItem('Books', JSON.stringify(newData));
-        window.location.reload()
-    }
-})
+bookshelf.addEventListener('click', (e) => {
+  if (e.target.classList.contains('removeBtn')) {
+    const index = parseInt(e.target.dataset.index, 10);
+    const bookRemove = document.querySelector(`[data-book="${index}"]`);
+    const newData = awesomeBooks.filter((_book, i) => i !== index);
+    localStorage.setItem('Books', JSON.stringify(newData));
+    bookshelf.removeChild(bookRemove);
+  }
+});
