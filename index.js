8bed7319f1df5f8bf8c7253749f52d0ff34c3e08
lordksix <wladimir.pasquel@urp.edu.pe>
2023-04-24 19:03:24 -0500

    Refactor dynamic functionality and add CSS

--- index.js
+++ index.js
@@ -1,114 +1,27 @@
-import renderModule from './indexModule.js';
-
-class Book {
-  constructor(Title, Author) {
-    this.Title = Title;
-    this.Author = Author;
-  }
-}
+import BBClass from './BookBinding.js';
 
 const add = document.getElementById('add');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const bookshelf = document.querySelector('.bookshelf');
+const localStorageName = 'Books';
+const bookDivName = 'book-row';
 
+const bookBinding = new BBClass.BookBinding(localStorageName);
 
-// // Displaying awesome books
-// const displayBooks = () => {
-//   const bookshelfFrag = document.createDocumentFragment();
-//   awesomeBooks.forEach((book, i) => {
-//     bookshelfFrag.appendChild(renderModule.createBookDiv(book.Title, book.Author, i));
-//   });
-//   bookshelf.appendChild(bookshelfFrag);
-// };
-
-// // addding to awesome books
-// const addBook = () => {
-//   const book = new Book(title.value.trim(), author.value.trim());
-//   const bookshelfArrLen = document.querySelectorAll('.bookshelf div').length;
-//   if (title.value.length && author.value.length) awesomeBooks.push(book);
-//   localStorage.setItem('Books', JSON.stringify(awesomeBooks));
-//   bookshelf.appendChild(renderModule.createBookDiv(title.value, author.value, bookshelfArrLen));
-//   title.value = '';
-//   author.value = '';
-// };
-
-// window.addEventListener('load', displayBooks);
-
-// // even listener on add button
-// add.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   addBook();
-// });
-
-// // removing from awesome books
-// bookshelf.addEventListener('click', (e) => {
-//   if (e.target.classList.contains('removeBtn')) {
-//     const index = parseInt(e.target.dataset.index, 10);
-//     const bookRemove = document.querySelector(`[data-book="${index}"]`);
-//     const newData = awesomeBooks.filter((_book, i) => i !== index);
-//     localStorage.setItem('Books', JSON.stringify(newData));
-//     bookshelf.removeChild(bookRemove);
-//   }
-// });
-
-class BookBinding {
-    constructor() {
-        this.awesomeBooks = localStorage.getItem('Books') ? JSON.parse(localStorage.getItem('Books')) : [];
-    }
-
-    add (title, author) {
-        const book = new Book(title.value, author.value)
-        if(title.value.length && author.value.length) {
-            this.awesomeBooks.push(book)
-        }
-        localStorage.setItem('Books', JSON.stringify(this.awesomeBooks));
-        title.value = '';
-        author.value = '';
-    }
-
-    removeBook (index) {
-        this.awesomeBooks.splice(index, 1)  
-        localStorage.setItem('Books', JSON.stringify(this.awesomeBooks));
-    }
-
-    static update () {
-        const awesomeBooks = JSON.parse(localStorage.getItem('Books'))
-        const bookElement = document.createElement('bookElement')
-        bookshelf.innerHTML = '';
-        awesomeBooks.forEach((book, i) => {
-            let color;
-            if (i % 2 === 0) {
-                color = 'bg-light'
-            } else {
-                color = '';
-            }
-            bookElement.innerHTML += `
-            <div id="items" class="d-flex justify-content-between align-items-center ${color}">
-                <p class="m-0">"${book.Title}" by ${book.Author}</p>
-                <button type="button" class="removeBtn btn btn-outline-dark p-1" data-index="${i}">Remove</button>
-            </div>
-            `;
-            bookshelf.appendChild(bookElement)
-        })
-    }
-}
-
-
-const bookBinding = new BookBinding()
-
-bookshelf.addEventListener('click', (event) => {
-    if(event.target.classList.contains('removeBtn')) {
-        const index = parseInt(event.target.dataset.index, 10)
-        bookBinding.removeBook(index)
-        BookBinding.update()
-    }
-})
+bookshelf.addEventListener('click', (e) => {
+  if (e.target.classList.contains('removeBtn')) {
+    const index = parseInt(e.target.dataset.index, 10);
+    const bookRemove = document.querySelector(`[data-book="${index}"]`);
+    bookBinding.removeBook(index, bookRemove, bookshelf);
+    bookshelf.innerHTML = '';
+    BBClass.BookBinding.update(bookshelf, localStorageName);
+  }
+});
 
 add.addEventListener('click', (e) => {
-    e.preventDefault();
-    bookBinding.add(title, author)
-    BookBinding.update();
-})
+  e.preventDefault();
+  bookBinding.add(title, author, bookshelf, bookDivName);
+});
 
-BookBinding.update()
+window.addEventListener('load', BBClass.BookBinding.update(bookshelf, localStorageName, bookDivName));
--- indexModule.js
+++ indexModule.js
@@ -1,28 +1,37 @@
-function createBookInfo(info) {
+const createBookInfo = (info, author) => {
   const pBook = document.createElement('p');
-  pBook.textContent = info;
+  pBook.textContent = `"${info}" by ${author}`;
   return pBook;
-}
+};
 
-function createButton(func, textContent, data, classes, index) {
+const createButton = (func, textContent, data, classes, index) => {
   const button = document.createElement('button');
   button.type = func;
   button.classList.add(`${classes}`);
   button.setAttribute(`data-${data}`, index);
   button.textContent = textContent;
   return button;
-}
+};
 
-function createBookDiv(title, author, index) {
+const createBookDiv = (title, author, index, classes) => {
   const div = document.createElement('div');
+  div.classList.add(`${classes}`);
   div.setAttribute('data-book', `${index}`);
-  div.append(createBookInfo(title), createBookInfo(author));
+  div.appendChild(createBookInfo(title, author));
   div.appendChild(createButton('button', 'Remove', 'index', 'removeBtn', index));
   return div;
+};
+
+class Book {
+  constructor(Title, Author) {
+    this.Title = Title;
+    this.Author = Author;
+  }
 }
 
 export default {
   createBookDiv,
   createButton,
   createBookInfo,
+  Book,
 };
